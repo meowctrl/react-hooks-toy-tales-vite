904059573c51e59d8701e3dbc0fa00286ce99265
carol <carolineallen1012@gmail.com>
2026-03-09 14:31:49 -0400

    commit 2

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,28 +1,42 @@
 import React from "react";
 
-function ToyCard({toy, onLike, onDonate, ...props}) {
-  function handleLike(e) {
-    if(onLike) {
-      onLike(toy, e);
-    }
-  };
+function ToyCard({ toy, onDelete, onLike }) {
+  const { id, name, image, likes } = toy;
 
-  function handleDonate(e) {
-    if(onDonate) {
-      onDonate(toy, e);
-    }
+  function handleDelete() {
+    fetch(`/toys/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      onDelete(id);
+    });
   }
+
+  function handleLike() {
+    const newLikes = likes + 1;
+    fetch(`/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ likes: newLikes }),
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => {
+        onLike(updatedToy);
+      });
+  }
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{toy?.name}</h2>
-      <img
-        src={toy?.image}
-        alt={toy?.name}
-        className="toy-avatar"
-      />
-      <p>{toy?.likes} Likes </p>
-      <button className="like-btn" onClick={handleLike}>Like {"<3"}</button>
-      <button className="del-btn" onClick={handleDonate}>Donate to GoodWill</button>
+      <h2>{name}</h2>
+      <img src={image} alt={name} className="toy-avatar" />
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={handleLike}>
+        Like &lt;3
+      </button>
+      <button className="del-btn" onClick={handleDelete}>
+        Donate to GoodWill
+      </button>
     </div>
   );
 }
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({toys, onLike, onDonate, ...props}) {
-  if (!toys?.length) {
-    return (
-      <div id="toy-collection">No toys currently.</div>
-    )
-  }
-  let cards = toys.map((toy, index) => {
-    return (
-      <ToyCard key={toy.id} toy={toy} onDonate={onDonate} onLike={onLike}/>
-    )
-  })
-
+function ToyContainer({ toys, onDeleteToy, onLikeToy }) {
   return (
-    <div id="toy-collection">{cards}</div>
+    <div id="toy-collection">
+      {toys.map((toy) => (
+        <ToyCard
+          key={toy.id}
+          toy={toy}
+          onDelete={onDeleteToy}
+          onLike={onLikeToy}
+        />
+      ))}
+    </div>
   );
 }
 
